10f48c8e9b26913eb418049bcb9f4d688de1281c
xxu29958-jpg <xxu29958@gmail.com>
2026-09-26 20:20:35 +0800

    test(planning): import history preview root selector

--- android/app/src/androidTest/java/com/ticketbox/ui/navigation/BudgetFirstUseRouteTest.kt
+++ android/app/src/androidTest/java/com/ticketbox/ui/navigation/BudgetFirstUseRouteTest.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.isRoot
 import androidx.compose.ui.test.junit4.v2.createComposeRule
+import androidx.compose.ui.test.onLast
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
